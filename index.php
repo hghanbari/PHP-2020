4d89d21cbdc10b18d5a089821079cf2cf55985a5
hghanbari <70950300+hghanbari@users.noreply.github.com>
2020-12-10 23:16:56 +0100

    hii

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<?php
-echo "Hello world";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Inhoudsopgave cursus PHP</title>
+</head>
+<body>
+<h1>PHP-2020</h1>
+<h3>Hamid Ghanbari</h3>
+<a href="h01/">Hoofstuk 1</a>
+</body>
+</html>
