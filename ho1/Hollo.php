6c2fdf7e8646a46d6069073d90ac4bd0b5df3663
hghanbari <70950300+hghanbari@users.noreply.github.com>
2020-12-11 13:52:43 +0100

    opdracht2 klaar

--- ho1/Hollo.php
+++ ho1/Hollo.php
@@ -1,2 +1,2 @@
 <?php
-echo "helll";
+echo "Hello World!";
---
+++ ho2/opdracht.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = "Ik heb niet gefaald.";
+$b = "die niet werken";
+$c = 10000;
+echo $a."Ik heb alleen ".$c." manieren gevonden ".$b."<br>"."-Thomas Edison<br>";
+
+
+$d = "die geen fout";
+$e = "en";
+$f = "niet.";
+echo "Mensen ".$d.$e."maken, werken".$f."<br>";
+echo "-Albert Plesman (KLM)<br>";
+
+$g = "ry";
+$h = ", but";
+$i = "fail";
+echo "T".$g."and ".$i."never".$i."to t".$g.".<br>";
+echo "-Tyra Banks<br>";
+
+$j = "“omgaan";
+$k = "met";
+$l = "teleu";
+echo "De cursus ".$j.$k.$l."rstellingen”"."kan vanavond helaas niet doorgaan.<br>";
+echo "- Herman Finkers<br>";
